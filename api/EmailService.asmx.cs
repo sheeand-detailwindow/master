e17bcced661738e30b50ece7593481f7ab1e3516
sheeand <sheeand@mytvtestdisc.com>
2014-01-21 05:10:50 -0500

    Building uc/EmailServices.asmx.cs

--- api/EmailService.asmx.cs
+++ api/EmailService.asmx.cs
@@ -4,10 +4,12 @@ using System.Configuration;
 using System.Data;
 using System.Data.OleDb;
 using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Services;
 using System.ServiceModel.Channels;
-using Newtonsoft.Json.Serialization;
+//using Newtonsoft.Json.Serialization;
 using System.Web.Caching;
 
 
@@ -52,7 +54,7 @@ namespace detailwindow.api
         }
         
         [WebMethod(Description = "This service has several uses, depending on the parameters given. For testing purposes, enter 'Reminder' for Type, 'WebmasterTest' for Rendition, and '1' for Row. This will send a test email to the webmaster.")]
-        public string SendEmail(string Type, string Rendition, string Row)
+        public List<string> SendEmail(string Type, string Rendition, string Row)
         {
             Dictionary<string, object> data = new Dictionary<string, object>();
             List<Dictionary<string, object>> dataList = new List<Dictionary<string, object>>();
@@ -75,67 +77,99 @@ namespace detailwindow.api
             bool SpecialsOptOut = Convert.ToBoolean(data["SpecialsOptOut"]);
             DateTime PromoSent = Convert.ToDateTime(data["PromoSent"]);
 
-            string test = String.Concat(LastName, " ", Email);
+            string strHeader = "<img src=\"cid:image1\"/><div style='font-family:Arial, Vernada;font-size:12px;'><br /><br />";
+            string strReminder = "<p style='font-size:16px; font-weight:bold;'>It's time to have your glass cleaned!<br /></p>";
+            string strDeepHome = "<p style='font-size:16px; font-weight:bold;'>Deep Home Cleaning<br /></p><p style='font-weight:bold;'>Your home will <span style='text-decoration:underline;'>FEEL</span> and <span style='text-decoration:underline;'>SMELL</span> clean&#33;</p><p><ul style='font-size:12px;'><li>Woodwork (baseboards, doors and frames)</li><li>Wood blinds and walls</li><li>Basements and garages</li><li>Under and behind major appliances, heavy furniture, and other hard-to-get places</li><li>Ceiling fans, chandeliers, mirrors, light fixtures, and more!</li></ul></p>";
+            string strSchedNow = "<p style='font-size:14px;'>Schedule your appointment now&#33;</p><p style='font-size:14px;'>Go to <a href='http://www.detailwindow.com'>www.DetailWindow.com</a>.<br />Or, call (317) 842-5326.</p>";
+            string strWinterStart = "<hr><p style='font-size:16px; font-weight:bold;'>Winter discounts on window cleaning&#33<ul style='font-size:12px;'>";
+            string strJanFeb = "<li>For work completed in January: 20% discount on window cleaning</li><li>For work completed in February: 20% discount on window cleaning</li>";
+            string strMarch = "<li>For work completed in March: 15% discount on window cleaning</li>";
+            string strWinterEnd = "</ul></p>";
+            string strJuly = "<p style='font-size:16px; font-weight:bold;'>Summer savings on window cleaning&#33<ul style='font-size:12px;'><li>For work completed in July: 10% discount on window cleaning</li></ul></p>";
+            string strFooter = "<p><span style='color:#aa0000; font-weight:bold;'>Detail Window Cleaning - RJJK, Inc.</span><br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#8226 Detail-minded professionals<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#8226 Serving greater Indianapolis</p><br /></div>";
 
+            string strSubject = "";
+            string strBody = "";
+            List<string> returnMessage = new List<string>();
+            returnMessage[0] = "Email sent.";
+
+            switch (Type)
+            {
+                case "Reminder":
+                    strSubject = "A Friendly Reminder";
+                    strBody = String.Concat(strHeader, strReminder, strSchedNow, strFooter);
+                    break;
+
+                case "JanFeb":
+                    strSubject = "Winter Discounts!";
+                    strBody = String.Concat(strHeader, strDeepHome, strSchedNow, strWinterStart, strJanFeb, strWinterEnd, strFooter);
+                    break;
+
+                case "March":
+                    strSubject = "Winter Discounts!";
+                    strBody = String.Concat(strHeader, strDeepHome, strSchedNow, strWinterStart, strMarch, strWinterEnd, strFooter);
+                    break;
+
+                case "July":
+                    strSubject = "Summer Savings!";
+                    strBody = String.Concat(strHeader, strJuly, strSchedNow, strFooter);
+                    break;
+            }
 
             switch (Rendition)
             {
                 case "Live":
-                    switch (Type)
-                    {
-                        case "Reminder":
-                            break;
-
-                        case "JanFeb":
-                            break;
-
-                        case "March":
-                            break;
-
-                        case "July":
-                            break;
-                    }
-
                     break;
 
                 case "WebmasterTest":
-                    switch (Type)
-                    {
-                        case "Reminder":
-                            break;
-
-                        case "JanFeb":
-                            break;
-
-                        case "March":
-                            break;
-
-                        case "July":
-                            break;
-                    }
-                    
+                    Send(strSubject, strBody, "sheeand@hotmail.com");
                     break;
 
                 case "AdministratorTest":
-                    switch (Type)
-                    {
-                        case "Reminder":
-                            break;
-
-                        case "JanFeb":
-                            break;
-
-                        case "March":
-                            break;
-
-                        case "July":
-                            break;
-                    }
-                    
                     break;
             }
 
-            return null;
+            return returnMessage;
+        }
+
+        private void Send(string subject, string strBody, string emailAddress)
+        {
+            SmtpClient client = new SmtpClient();
+            MailMessage mail = new MailMessage();
+            string strpath = Server.MapPath(@"detailLogoMini.gif");
+            LinkedResource logo = new LinkedResource(strpath, MediaTypeNames.Image.Gif);
+            logo.ContentId = "image1";
+            logo.ContentType.Name = "detailLogoMini.gif";
+            AlternateView av1 = AlternateView.CreateAlternateViewFromString(strBody, null, MediaTypeNames.Text.Html);
+            av1.LinkedResources.Add(logo);
+            mail.AlternateViews.Add(av1);
+            client.Host = ConfigurationManager.AppSettings["SmtpServer"];
+            mail.From = new MailAddress(ConfigurationManager.AppSettings["MailFrom"]);
+            mail.To.Add(emailAddress);
+            mail.IsBodyHtml = true;
+            mail.Subject = subject;
+            if (ConfigurationManager.AppSettings["SmtpServer"] == "smtp.comcast.net")
+            {
+                client.Port = 587;
+                client.UseDefaultCredentials = false;
+                System.Net.NetworkCredential cred = new System.Net.NetworkCredential("sheeand", "MSLKQcg");
+                client.Credentials = cred;
+            }
+            using (client)
+            {
+                try
+                {
+                    client.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    Session["ErrorCode"] = ex.Message;
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+            }
         }
 
         public string SendAnotherEmail(string Type, string Rendition, string Row)
